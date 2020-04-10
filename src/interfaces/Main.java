3cd3905248646a30bb9e74958856bb2f3ae995a2
aravindh-venkatesh <v.aravindh@gmail.com>
2020-04-10 22:43:03 +0530

    Interface examples

---
+++ src/interfaces/Main.java
@@ -0,0 +1,20 @@
+package interfaces;
+
+public class Main implements TypesOfMethodsAndAttributes {
+    // public non static methods need to over-ridden.
+    // default methods can be over-ridden or used as it is
+
+    @Override
+    public void method1() {
+        System.out.println("Method 1");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Pi: " + TypesOfMethodsAndAttributes.pi);
+
+        Main main = new Main();
+        main.method1();
+        TypesOfMethodsAndAttributes.method2();
+        main.method3();
+    }
+}
---
+++ src/interfaces/TypesOfMethodsAndAttributes.java
@@ -0,0 +1,35 @@
+package interfaces;
+
+public interface TypesOfMethodsAndAttributes {
+    // implicitly attributes are static. it is not required to implement an interface to use static attributes
+    double pi = 3.14;
+
+    // most generic form of method written for interface
+    // this method will be implemented by classes implementing interface
+    // implicitly methods are public
+    void method1();
+
+    // static method; similar to attributes; need not require to implement interface to use static methods
+    static void method2() {
+        System.out.println("Method 2");
+    }
+
+    // default methods provide implementation
+    // helpful to prevent breaking legacy code whose interface require addition of methods
+    // default methods can call private methods (as private methods all provide implementation)
+    default void method3() {
+        System.out.println("Method 3");
+        method4();
+    }
+
+    // private methods are not visible outside interface
+    // only default methods and private methods can call other private methods
+    private void method4() {
+        method5();
+    }
+
+    private void method5() {
+        System.out.println("Method 4");
+    }
+
+}
